186f9162a598497f3c3080d84426aafb591084ae
kp-jumpei-onishi <kpjumpeionishi@gmail.com>
2025-01-17 13:30:04 +0900

    不要なファイル削除

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>ホーム画面</h1>
   </main>
 </template>
